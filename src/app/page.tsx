6bc6cb103344753f9e1d84ce3871198df9f9cf60
Juan Cabana <juan.cabana@pragma.com.co>
2025-04-09 13:00:35 -0500

    chore: MUI library added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
-  );
+  return <div>main page</div>;
 }
